cd070a9b8bc34a8fa66fd88dd0ced0fc97fa612e
Cedrus81 <erez.eitan.97@gmail.com>
2023-01-19 17:35:40 +0200

    initial commit. Just so it can be on github-pages

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen.js';
+import { auth } from './firebase';
 function App() {
+  const user = null
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) { } else { }
+    });
+    return unsubscribe
+  }, [user])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) :
+          (<Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>)}
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
