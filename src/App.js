e2fcaea738225796664ac6d624b2440ac80b4006
Cedrus81 <erez.eitan.97@gmail.com>
2023-01-19 18:02:59 +0200

    added login/logout functionality

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen.js';
 import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { logout, login } from './features/userSlice';
 function App() {
   const user = null
+  const dispatch = useDispatch()
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      if (userAuth) { } else { }
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
     });
     return unsubscribe
-  }, [user])
+  }, [])
 
   return (
     <div className="app">
